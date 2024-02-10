3ec37d0d8225f673bb9f2e8bd30ba64db386dc31
ChristianGWalsh <cgwalsh@loyola.edu>
2024-02-10 13:37:41 -0500

    Did some stuff to some data. check svd.py

--- svd.py
+++ svd.py
@@ -1,80 +1,74 @@
+import pandas
 import numpy
-from numpy.linalg import svd
-from numpy.linalg import multi_dot
-from numpy.linalg import norm
+import datetime
 
-# A = the array of stuff
-#not the correct data just testing
+#Program acts as a simple recommender system
+#culmination of the exploration i've been doing into SVD and recomender systems
+#svd is used as my 
+#take in the movie data as a pd dataframe / delim is split / names is cols
+dataMan = pandas.read_csv('/Users/ChristianWalsh/Downloads/ml-100k/u.data', delimiter='\t',names=['UserID', 'ItemID', 'Rating', 'Timestamp'])
+#print(dataMan.head)
 
-A = numpy.array([
-    [1,-0.8],
-    [0,1],
-    [1,0]
-])
+#METADATA YAY (for movies) / all seen categories from u.genre
+colNames = ['movie_id' , 'movie_title' , 'release_date' , 'video_release_date' ,
+             'IMDb_URL' , 'unknown', 'Action', 'Adventure', 'Animation',
+             'Childrens', 'Comedy', 'Crime' , 'Documentary', 'Drama', 'Fantasy',
+             'Film-Noir', 'Horror', 'Musical', 'Mystery' , 'Romance' , 'Sci-Fi',
+             'Thriller', 'War' ,'Western']
+#movies dataframe 
+movies = pandas.read_csv('/Users/ChristianWalsh/Downloads/ml-100k/u.item', delimiter='|', encoding='latin1', names=colNames)
+#print(movies.head())
 
-# i can print(A)
-# A.shape tells me rows and cols
+#creating my sparse matrix A
+userRatingPerItem = dataMan.pivot(index='UserID', columns='ItemID', values='Rating')
+#print(userRatingPerItem.head())
 
-#doing the svd calc splitting A into U S VT
-#full matrices for this case, change if not full
-U, S, VT = svd(A, full_matrices=True)
-#in this case, U will be one row shorter if i use false
+#loop through the sparce matrix and fill it in with the mean of the column
+for column in userRatingPerItem:
+    mean = userRatingPerItem[column].mean() #taking the mean for each col
+    userRatingPerItem[column] = userRatingPerItem[column].fillna(value=mean) #fillna - any empty only
+#print(userRatingPerItem.head())
 
-#U contains left singular vectors
-#S contains singular vaules
-#VT contains the right singular vaules
+#a user matrix is now needed to calculate the "distance" or relativity between each user
+#this method uses the Euclidean distance. Other methods like cosine similarity may also be used 
+#THIS PORTION WAS "BORROWED" from a github repo i studied
 
-#To display going from A(mxn) to U(mxm) S(mxn) VT(nxn)
-#print(U.shape, S.shape, VT.shape)
-#S is 1 dimensional hence the (2,)
+#DISTANCE
+def distance(v1,v2):
+    return numpy.sqrt(numpy.sum((v1-v2)**2))
+u1 = userRatingPerItem.iloc[1]
+u2 = userRatingPerItem.iloc[2]
+distance(u1,u2)
 
-#----------
-#Turning U, S, and VT back into A
-#making a 2d array of 0's to put my 1d S into
-smat = numpy.zeros((3,2)) #size same as A
-smat[:2, :2] = numpy.diag(S) #putting the S values into the 0 array
-#print(smat)
-print()
+#This takes a while to run
+start = datetime.datetime.now()
+userMatrix = []
+for i, row in enumerate(userRatingPerItem.index):
+    u1 = userRatingPerItem[row]
+    #symmetric matrix fill all values previously examined
+    userDistanes = [entry[i] for entry in userMatrix]
+    for j, row2 in enumerate(userRatingPerItem.index[i:]):
+        u2 = userRatingPerItem[row2]
+        dist = distance(u1,u2)
+        userDistanes.append(dist)
+    userMatrix.append(userDistanes)
+userSimilarities = pandas.DataFrame(userMatrix)
+end = datetime.datetime.now()
+elapsed = end - start
+#print(elapsed)
+#print(userSimilarities.head())
 
-#use multidot to multiply the 3 matrices and get A
-print(multi_dot([U,smat,VT]))
+#now all that is left to do is make a recommend function to rec movies to user
+def recommendMovies(user, userSimilarities, userRatingPerItem, dataMan, nUsers=20, nItems=10):
+    #n is num of similar uses i want to gen data for
+    #User similarities are offset by 1 so remove the current user
+    topNSimilarUsers = userSimilarities[user-1].drop(user-1).sort_values().index[:nUsers]
+    #offset fix here
+    topNSimilarUsers = [i+1 for i in topNSimilarUsers]
+    alreadyWatched = set(dataMan[dataMan.UserID == 0].ItemID.unique())
+    unseen = set(dataMan.ItemID.unique()) - alreadyWatched
+    projectedReviews = userRatingPerItem[userRatingPerItem.index.isin(topNSimilarUsers)].mean()[list(unseen)].sort_values(ascending=False)
+    return projectedReviews[:nItems]
 
-#to compare my remade A to the og matrix, i can take the "norm"
-print(norm(A - multi_dot([U,smat,VT])))
-#this number is basically 0, so I can consider it equal to the og
-
-#-----------
-#LOW RANK APPROX 
-#great for compressing data and getting rid of useless aspects
-#ex array
-B = numpy.array([
-    [.1649, .6164, .2730],
-    [.2317, .8663, .3837],
-    [.0334, .1249, .0553],
-    [.2284, .8537, .3781],
-    [.0769, .2874, .1273]
-])
-
-U1, S1, VT1 = svd(B, full_matrices=False) #FALSE cuz low rank
-#print(U1.shape, S1.shape, VT1.shape)
-
-#can see that the first singular value carries much more 
-#importance than the rest, so just use the first
-#this IS low rank approx
-print(S1)
-
-rank =1
-U1_sub = U1[:,:rank]
-VT1_sub = VT1[:rank,:]
-S1_sub = numpy.diag(S1[:rank])
-#using .dot here for just 2 matrix multi
-B_low_rank = numpy.dot(numpy.dot(U1_sub, S1_sub), VT1_sub)
-
-#use the norm to see how accurate it is
-print(norm(B-B_low_rank))
-#same thing, if basically 0, all is good
-
-
-
-
-
+#example of some recs
+print(recommendMovies(1, userSimilarities, userRatingPerItem, dataMan))
