e49256c12c465d88e5ca5c49a97719035f8bcea1
ChristianGWalsh <cgwalsh@loyola.edu>
2024-02-12 16:36:01 -0500

    j cleaning up a little bit

--- svd.py
+++ svd.py
@@ -1,25 +1,16 @@
 import pandas
 import numpy
-import datetime
 
 #Program acts as a simple recommender system
 #culmination of the exploration i've been doing into SVD and recomender systems
-#svd is used as my 
+
 #take in the movie data as a pd dataframe / delim is split / names is cols
 dataMan = pandas.read_csv('/Users/ChristianWalsh/Downloads/ml-100k/u.data', delimiter='\t',names=['UserID', 'ItemID', 'Rating', 'Timestamp'])
 #print(dataMan.head)
 
-#METADATA YAY (for movies) / all seen categories from u.genre
-colNames = ['movie_id' , 'movie_title' , 'release_date' , 'video_release_date' ,
-             'IMDb_URL' , 'unknown', 'Action', 'Adventure', 'Animation',
-             'Childrens', 'Comedy', 'Crime' , 'Documentary', 'Drama', 'Fantasy',
-             'Film-Noir', 'Horror', 'Musical', 'Mystery' , 'Romance' , 'Sci-Fi',
-             'Thriller', 'War' ,'Western']
-#movies dataframe 
-movies = pandas.read_csv('/Users/ChristianWalsh/Downloads/ml-100k/u.item', delimiter='|', encoding='latin1', names=colNames)
-#print(movies.head())
 
-#creating my sparse matrix A
+#creating my sparse "dense" matrix A 
+#PIVOTED DATA 
 userRatingPerItem = dataMan.pivot(index='UserID', columns='ItemID', values='Rating')
 #print(userRatingPerItem.head())
 
@@ -31,17 +22,15 @@ for column in userRatingPerItem:
 
 #a user matrix is now needed to calculate the "distance" or relativity between each user
 #this method uses the Euclidean distance. Other methods like cosine similarity may also be used 
-#THIS PORTION WAS "BORROWED" from a github repo i studied
 
-#DISTANCE
 def distance(v1,v2):
     return numpy.sqrt(numpy.sum((v1-v2)**2))
 u1 = userRatingPerItem.iloc[1]
 u2 = userRatingPerItem.iloc[2]
 distance(u1,u2)
 
+#THIS PORTION WAS "BORROWED" from a github repo i studied
 #This takes a while to run
-start = datetime.datetime.now()
 userMatrix = []
 for i, row in enumerate(userRatingPerItem.index):
     u1 = userRatingPerItem[row]
@@ -53,9 +42,6 @@ for i, row in enumerate(userRatingPerItem.index):
         userDistanes.append(dist)
     userMatrix.append(userDistanes)
 userSimilarities = pandas.DataFrame(userMatrix)
-end = datetime.datetime.now()
-elapsed = end - start
-#print(elapsed)
 #print(userSimilarities.head())
 
 #now all that is left to do is make a recommend function to rec movies to user
